9ebf30c6c1bc23eb19a344539e1a1f852ce4f9ad
Frankie Molina <frankie.molina1411@gmail.com>
2026-05-20 23:15:00 -0700

    setup lesson-7 branch, add js file, and start coding challenge

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+// Add footer
+const footer = document.createElement('footer');
+// footer.innerText = "This is the footer"; just testing to make sure it works. 
+
+document.body.append(footer);
+
+// Creating copyright text
+const today = new Date();
+console.log(today)
