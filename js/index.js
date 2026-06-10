59552ef50f6654e5716158f775bc01857b8c0646
Frankie Molina <frankie.molina1411@gmail.com>
2026-06-10 15:41:21 -0700

    cleaning up code

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ selectFooter.append(copyright);
 
 // build skills array via DOM 
 const skills = ["JavaScript", "DOM", "HTML", "CSS"];
-const skillsSection = document.querySelector('#Skills');
+const skillsSection = document.querySelector('#skills');
 // console.log(skillsSection);
 
 const skillsList = skillsSection.querySelector('ul');
