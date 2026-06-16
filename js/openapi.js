364e753724830f0b1df5af927fe2c2cc1c89dcff
Frankie Molina <frankie.molina1411@gmail.com>
2026-06-16 12:57:09 -0700

    fix data display, accessibility, and semantic markup

--- js/openapi.js
+++ js/openapi.js
@@ -29,20 +29,34 @@ async function fetchEstadioAztecaTemperature() {
     
     const data = await response.json();
     // console.log(data);
-    const time = data.hourly.time[0];
-    const temperature = data.hourly.temperature_2m[0];
-    // console.log(`${time}: ${temperature}`)
-    const result = `${time}: ${temperature}`
 
+
+    // These are unnecessary since we are using a dynamic function instead. 
+    // const time = data.hourly.time[0];
+    // const temperature = data.hourly.temperature_2m[0];
+    // // console.log(`${time}: ${temperature}`)
+    // const result = `${time}: ${temperature}`
+
+
+    // This was moved up to the global scope so as to avoid repetitiveness.
     // const weatherInfo = document.querySelector('#weather_Info');
+
 
     // line 40 clears the list every time before adding new data.
     weatherInfo.innerHTML = '';
 
-    const addInfo = document.createElement('li');
 
+
+    // AIRHUB wants a dynamic API call. So I am adding a forEach loop to get current data. 
+    data.hourly.time.forEach((time, index) => {
+    const temperature = data.hourly.temperature_2m[index];
+    const result = `${time}: ${temperature}`;
+    const addInfo = document.createElement('li');
     addInfo.innerHTML = result;
     weatherInfo.append(addInfo);
+    });
+
+
 
   } catch (error) {
     console.error('An error occurred:', error);
@@ -68,18 +82,29 @@ async function fetchEstadioAztecaWindSpeed() {
     
     const data = await response.json();
     // console.log(data);
-    const time = data.hourly.time[0];
-    const windSpeed = data.hourly.wind_speed_10m[0];
-    // console.log(`${time}: ${windSpeed}`)
-    const result = `${time}: ${windSpeed}`;
+
+
+
+    // These are unneccesary since AIRHUB wanted a dynamic response instead of a hard-coded response. 
+    // const time = data.hourly.time[0];
+    // const windSpeed = data.hourly.wind_speed_10m[0];
+    // // console.log(`${time}: ${windSpeed}`)
+    // const result = `${time}: ${windSpeed}`;
 
       // line 40 clears the list every time before adding new data.
     weatherInfo.innerHTML = '';
 
+    // AIRHUB wants a dynamic API call. So I am adding a forEach loop to get current data. 
+    data.hourly.time.forEach((time, index) => {
+    const windSpeed = data.hourly.wind_speed_10m[index];
+    const result = `${time}: ${windSpeed}`;
     const addInfo = document.createElement('li');
-
     addInfo.innerHTML = result;
     weatherInfo.append(addInfo);
+    });
+
+
+
 
   } catch (error) {
     console.error('An error occurred:', error);
@@ -96,4 +121,10 @@ getWindSpeed.addEventListener('click', e => {
 
 // 2. Clicking buttons repeatedly appends duplicate entries to the DOM
 // Future fix: add a conditional to check if the ul already has a child node before appending
+
+
+
+
+// I fixed the know limitations. AIRHUB called them out. I didn't think to clear the inner HTML. That is much simpler to do than writing the for loop I had thought about writing haha. 
+
 
